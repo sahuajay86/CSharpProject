290bb0d31702e163ce2a81f321d0db78a28ae8a2
sahuajay86 <sahu_ajay1986@yahoo.com>
2017-11-26 08:19:39 +0530

    First Project Commit

---
+++ GoExpert/IService1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+namespace GoExpert
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+
+        [OperationContract]
+        GoExpert test(GoExpert exp);
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+    [DataContract]
+    public class GoExpert
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["goexpertconnection"]);
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        SqlDataReader dr;      
+       
+        public string test ()
+        {
+            using (con)
+            {
+                con.Open();
+                using (cmd)
+                {
+                    cmd.CommandText = "select * from Mst_State";
+                    cmd.Connection = con;
+                    cmd.ExecuteNonQuery();
+                }
+                con.Close();
+            }
+            return "";
+        }
+
+    }
+
+}
---
+++ GoExpert/Service1.svc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GoExpert
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+         public GoExpert test(GoExpert x)
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["goexpertconnection"]);
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        SqlDataReader dr;
+
+      
+            using (con)
+            {
+                con.Open();
+                using (cmd)
+                {
+                    cmd.CommandText = "select * from Mst_State";
+                    cmd.Connection = con;
+                    cmd.ExecuteNonQuery();
+                }
+                con.Close();
+            }
+            return x;
+        }
+
+    } 
+    }
